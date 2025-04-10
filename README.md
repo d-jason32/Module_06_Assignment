a184866469324658ed97c058a56e54acf0e6593c
Jason Devaraj <jdevaraj01@gmail.com>
2025-04-10 19:02:33 -0400

    Added checks.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Regex Assignment 
+![img.png](img.png)
--- src/main/java/edu/farmingdale/module_06_assignment/HelloController.java
+++ src/main/java/edu/farmingdale/module_06_assignment/HelloController.java
@@ -2,33 +2,102 @@ package edu.farmingdale.module_06_assignment;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
 import java.util.regex.*;
 
 import java.io.IOException;
 
-public class HelloController {
+public class HelloController{
+    @FXML
+    private TextField dob;
 
+    @FXML
+    private TextField email;
 
-    Pattern name = Pattern.compile("^[a-zA-Z]{2,25}$");
+    @FXML
+    private TextField firstName;
 
-    Pattern dob = Pattern.compile("^(0[1-9]|1[0-2])/([0][1-9]|[12][0-9]|3[01])/\\d{4}$");
+    @FXML
+    private TextField lastName;
 
-    Pattern email = Pattern.compile("^[a-zA-Z0-9._%+-]{1,25}@farmingdale\\.edu$");
+    @FXML
+    private StackPane loginBox;
 
-    Pattern zipcode = Pattern.compile("^\\d{5}$");
+    @FXML
+    private Button registerButton;
 
-    Matcher m = dob.matcher("Now is the time"); // entire string
+    @FXML
+    private TextField zipcode;
 
-    //Matcher a = dob.find("Now is the time");
+    @FXML
+    private Label dobCheck;
+
+    @FXML
+    private Label emailCheck;
+
+    @FXML
+    private Label firstNameCheck;
+
+    @FXML
+    private Label lastNameCheck;
+
+    @FXML
+    private Label zipcodeCheck;
+
+    // Name field can only contain alphabetical characters and must be a length 2-25
+    Pattern namePattern = Pattern.compile("^[a-zA-Z]{2,25}$");
+    // Date of birth must be in format MM/DD/YYYY
+    Pattern dobPattern = Pattern.compile("^(0[1-9]|1[0-2])/([0][1-9]|[12][0-9]|3[01])/\\d{4}$");
+    // Email must contain @farmingdale.edu
+    Pattern emailPattern = Pattern.compile("^[a-zA-Z0-9._%+-]{1,25}@farmingdale\\.edu$");
+    // Zipcode must be a number with 5 digits
+    Pattern zipcodePattern = Pattern.compile("^\\d{5}$");
+
+    //Matcher m = dob.matcher("Now is the time"); // entire string
 
     @FXML
     void goToPage(ActionEvent event) throws IOException {
-        HelloApplication.setRoot("page2");
+        checkIfCorrect();
+        //HelloApplication.setRoot("page2");
     }
 
-    // First and Last name: A-Z only, 2-25 inclusive
-    // DOB MM/DD/YYYY
-    //Email only accept farmingdale email address
-    // zipcode numbers 5 digits
+    void checkIfCorrect(){
+        if (namePattern.matcher(firstName.getText()).matches()){
+            firstNameCheck.setText("✅");
+        }
+        else {
+            firstNameCheck.setText("❌");
+        }
+        if (namePattern.matcher(lastName.getText()).matches()){
+            lastNameCheck.setText("✅");
+        }
+        else {
+            lastNameCheck.setText("❌");
+        }
+        if (dobPattern.matcher(dob.getText()).matches()){
+            dobCheck.setText("✅");
+        }
+        else {
+            dobCheck.setText("❌");
+        }
+        if (emailPattern.matcher(email.getText()).matches()){
+            emailCheck.setText("✅");
+        }
+        else {
+            emailCheck.setText("❌");
+        }
+        if (zipcodePattern.matcher(zipcode.getText()).matches()){
+            zipcodeCheck.setText("✅");
+        }
+        else {
+            zipcodeCheck.setText("❌");
+        }
+    }
+
+    // inform the validity of each input when they move focus to the next field
+    // enable add button only if all fields contain correct data otherwise it remains greyed out
+    // only once everything is validated allow user to click on add button
 }
