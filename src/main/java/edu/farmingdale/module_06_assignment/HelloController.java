a6a0ccec33d5c9688c9a047c04887ae034caaaf8
Jason Devaraj <jdevaraj01@gmail.com>
2025-04-10 19:42:17 -0400

    Add comments.

--- src/main/java/edu/farmingdale/module_06_assignment/HelloController.java
+++ src/main/java/edu/farmingdale/module_06_assignment/HelloController.java
@@ -9,7 +9,7 @@ import javafx.scene.layout.StackPane;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.regex.*;
-// Initializable needed to implement initializable
+// Initializable needed to implement initialize
 import javafx.fxml.Initializable;
 import java.io.IOException;
 
@@ -38,6 +38,8 @@ public class HelloController implements Initializable{
     private Label lastNameCheck;
     @FXML
     private Label zipcodeCheck;
+    boolean firstNameBool, lastNameBool, dobBool, emailBool, zipcodeBool = false;
+    boolean canRegister = false;
 
     // Name field can only contain alphabetical characters and must be a length 2-25
     Pattern namePattern = Pattern.compile("^[a-zA-Z]{2,25}$");
@@ -48,57 +50,115 @@ public class HelloController implements Initializable{
     // Zipcode must be a number with 5 digits
     Pattern zipcodePattern = Pattern.compile("^\\d{5}$");
 
-
+    /**
+     * Only if all text fields are valid, you can go to the next page.
+     * @param event
+     * @throws IOException
+     */
     @FXML
     void goToPage(ActionEvent event) throws IOException {
-        HelloApplication.setRoot("page2");
+        if (canRegister == true){
+            HelloApplication.setRoot("page2");
+        }
     }
 
+    /**
+     * Checks if every text field is valid.
+     */
     void checkIfCorrect(){
         if (namePattern.matcher(firstName.getText()).matches()){
             firstNameCheck.setText("✅");
+            firstNameBool = true;
         }
         else {
             firstNameCheck.setText("❌");
+            firstNameBool = false;
         }
         if (namePattern.matcher(lastName.getText()).matches()){
             lastNameCheck.setText("✅");
+            lastNameBool = true;
         }
         else {
             lastNameCheck.setText("❌");
+            lastNameBool = false;
         }
         if (dobPattern.matcher(dob.getText()).matches()){
             dobCheck.setText("✅");
+            dobBool = true;
         }
         else {
             dobCheck.setText("❌");
+            dobBool = false;
         }
         if (emailPattern.matcher(email.getText()).matches()){
             emailCheck.setText("✅");
+            emailBool = true;
         }
         else {
             emailCheck.setText("❌");
+            emailBool = false;
         }
         if (zipcodePattern.matcher(zipcode.getText()).matches()){
             zipcodeCheck.setText("✅");
+            zipcodeBool = true;
         }
         else {
             zipcodeCheck.setText("❌");
+            zipcodeBool = false;
         }
     }
 
+    /**
+     * If every field is valid, the canRegister is set to true.
+     */
+    void setRegisterButton(){
+        if (firstNameBool == true
+                && lastNameBool == true
+                && dobBool == true
+                && emailBool == true
+                && zipcodeBool == true){
+            canRegister = true;
+        }
+        else {
+            canRegister = false;
+        }
+    }
+
+    /**
+     * If every text field is valid, the button will change from grey to green.
+     */
+    void changeColor(){
+        if (canRegister){
+            registerButton.setStyle("-fx-background-color: #24ff2b; -fx-text-fill: white;");
+        }
+        else {
+            registerButton.setStyle("-fx-background-color: #a9a9a9;; -fx-text-fill: white;");
+        }
+    }
+
+    /**
+     * Method that runs when the file screen is showing.
+     * When a text field is clicked on, it will check if
+     * it is correct, check if the register button should be
+     * enabled, and change the color of the button if
+     * every text field is correct.
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         TextField[] fields = { firstName, lastName, email, dob, zipcode };
 
+        /*
+        After each text field is clicked on, every
+        field will be checked if it is correct.
+         */
         for (TextField field : fields) {
             field.setOnMouseClicked(e -> {
                 checkIfCorrect();
+                setRegisterButton();
+                changeColor();
             });
         }
     }
-
-    // inform the validity of each input when they move focus to the next field
-    // enable add button only if all fields contain correct data otherwise it remains greyed out
-    // only once everything is validated allow user to click on add button
 }
